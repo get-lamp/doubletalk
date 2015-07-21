37132e22ecfc2939ae5de21a5fff627d15faea7c
64kbytes <64kgames@gmail.com>
2015-07-20 23:17:03 -0300

    Some work done

--- python/dt.py
+++ python/dt.py
@@ -2,8 +2,22 @@
 
 import StringIO, re
 
-class Syntax(object):
+class Doubletalk(object):
 	DELIMITERS 			= "[.:!,;+*^&'\"\-\\/\|=@#$%()?<>\s]"
+	tree = {
+		'[/]': {
+			'[*]': 'comment_block_open',
+			'[/]': 'comment_line',
+		},
+		'[*]': {
+			'[/]': 'comment_block_close'
+		},
+		'[=]': {
+			'[=]': {
+				'[=]': 'equal_strict'
+			}
+		}
+	}
 	
 class Scanner(object):
 	
@@ -15,11 +29,13 @@ class Scanner(object):
 	def scan(self):
 	
 		if self.src.closed:
+			# already at EOF
 			return None
 
 		while True:
 			char = self.src.read(1)
 			if not char:
+				# EOF
 				self.src.close()
 				break
 			
@@ -58,23 +74,61 @@ class Lexer(object):
 	def parse(self):
 		while True:
 			token = self.scanner.scan()
+						
+			for regexp in self.syntax.tree:
+				if re.match(regexp, token):
+					print token
 			
 			if token == '"':
 				return ''.join(self.verbatim('"'))
 		
 
-#lex = Lexer(Syntax(), 'sample.dtk').parse()			
+lex = Lexer(Doubletalk(), 'sample.dtk').parse()			
 #print lex
 
-grammar = {
-	"[/]": {
-		"[*]": {
-			".": None
-		}
-	}
-}
+"""
+def scan(self):
+	
+		if self.src.closed:
+			# already at EOF
+			return None
+			
+		branch = self.syntax.tree
+		self.token = []
 
-print grammar['[/]']
+		while True:
+			char = self.src.read(1)
+			
+			if not char:
+				# EOF reached
+				self.src.close()
+				break
+				
+			for regexp in branch:
+				c = re.match(regexp, char)
+				if c is not None:
+					self.token.append(char)
+				
+					if isinstance(branch[regexp], dict):
+						branch = branch[regexp]
+					elif branch[regexp] == 0:
+						return ''.join(self.token) if len(self.token) > 0 else None
+					else:
+						p = len(branch) - branch[regexp]
+						while len(branch) > p:
+  							branch.pop()
+  				else:
+  					break
+  						
+  							
+		return ''.join(self.token) if len(self.token) > 0 else None	
+					
+
+"""
+
+
+
+
 
 
 
