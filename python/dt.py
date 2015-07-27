ca8f6fd961cb9ecdfbb63972fae2749bb3662516
64kbytes <64kgames@gmail.com>
2015-07-26 22:45:48 -0300

    On to the Parser

--- python/dt.py
+++ python/dt.py
@@ -3,39 +3,89 @@
 import StringIO, re
 
 class Doubletalk(object):
-	DELIMITERS = "[.:!,;+*^&'\"\-\\/\|=$()?<>\s]"
+
+	class Lexeme(object):
+		def __init__(self, word, **kwargs):
+			self.word = word
+
+	class PreprocessorDirective(Lexeme):
+		pass
+		
+	class CommentBlock(PreprocessorDirective):
+		pass
+	
+	class CommentLine(PreprocessorDirective):
+		pass
+	
+	class Identifier(Lexeme):
+		pass
+		
+	class String(Lexeme):
+		pass
+	
+	class Number(Lexeme):
+		pass
+	
+	class Operator(Lexeme):
+		pass
+	
+	class Keyword(Lexeme):
+		pass
+	
+	delimiters = "[.:!,;+*^&@#$%&'\"\-\\/\|=$()?<>\s]"
+	
+	r_space 		= r'[ ]'
+	r_newline		= r'[\n]'
+	r_tab 			= r'[\t]'
+	r_slash 		= r'[/]'
+	r_asterisk 		= r'[*]'
+	r_equal 		= r'[=]'
+	r_plus 			= r'[+]'
+	r_dash 			= r'[-]'
+	r_number 		= r'^[0-9]+'
+	r_identifier 	= r'[_a-zA-Z][_a-zA-Z0-9]*'
+	r_atsign		= r'[@]'
+	r_double_quote 	= r'["]'
+	r_single_quote 	= r'[\']'
 	
 	symbols = {
-		r'[\s]': 	'[space]',
-		r'^\n$':	'[newline]',
-		r'[\t]':	'[tab]',
-		r'[/]': {
-			r'[*]': '[comment_block_open]',
-			r'[/]': '[comment_line]',
-			None:	'[divide]'
+		r_space: 		'[space]',
+		r_newline:		'[newline]',
+		r_tab:			'[tab]',
+		r_double_quote: '[string]',
+		r_single_quote: '[string]',
+		r_slash: {
+			r_asterisk:	lambda s: Doubletalk.CommentBlock(s, open=True),
+			r_slash: 	'[comment_line]',
+			None:		'[divide]'
 		},
-		r'[*]': {
-			r'[/]':	'[comment_block_close]',
-			None:	'[multiply]'
+		r_asterisk: {
+			r_slash:	lambda s: Doubletalk.CommentBlock(s, open=False),
+			None:		'[multiply]'
 		},
-		r'[=]': {
-			r'[=]': {
-				r'[=]': '[equal_strict]',
-				None:	'[equal]'
+		r_equal: {
+			r_equal: {
+				r_equal:	'[equal_strict]',
+				None:		'[equal]'
 			},
-			None: '[assign]'
+			None: 			'[assign]'
 		},
-		r'[0-9]': 						'[number]',
-		r'[_a-zA-Z][_a-zA-Z0-9]*':		'[identifier]',
-		r'[@][_a-zA-Z][_a-zA-Z0-9]*':	'[character]',
-		r'[#][_a-zA-Z][_a-zA-Z0-9]*':	'[place]',
-		r'[%][_a-zA-Z][_a-zA-Z0-9]*':	'[mobile]',
-		r'[&][_a-zA-Z][_a-zA-Z0-9]*':	'[item]'
+		r_plus: {
+			r_plus:	'[increment]',
+			None:	'[add]'
+		},
+		r_dash: {
+			r_dash:		'[decrement]',
+			None:		'[subtract]'
+		},
+		r_number: 			'[number]',
+		r_identifier:		lambda i: Doubletalk.Identifier(i),
+		r_atsign: {
+			r_identifier:	'[character]',
+			None: '[ego]'
+		}
 	}
 
-	grammar = {
-
-	}
 	
 class Lexer(object):
 	
@@ -43,6 +93,8 @@ class Lexer(object):
 		self.src	= open(source) if is_file else StringIO.StringIO(source)
 		self.syntax	= syntax
 		self.token	= []
+		self.nline	= 0
+		self.nchar 	= 0
 
 	def __exit__(self):
 		self.src.close()
@@ -52,30 +104,46 @@ class Lexer(object):
 		if self.src.closed:
 			# already at EOF
 			return None
+		
+		self.token = []
 
 		while True:
 			# read character
 			char = self.src.read(1)
+			
 			if not char:
 				# EOF
 				self.src.close()
 				break
 			
-			c = re.match(self.syntax.DELIMITERS, char)
+			c = re.match(self.syntax.delimiters, char)
 			
-			# no delimiter found
+			# no delimiter found. Keep reading
 			if c is None:
 				self.token.append(char)
 			else:
+				# a delimiter found. A single char to return
 				if len(self.token) == 0:
-					return char
+					self.token = char
+					break
+				# a multichar token to return. 
+				# backtrack 1 to leave pointer in position for next reading
 				else:
 					self.src.seek(-1, 1)
-					tkn = ''.join(self.token)
-					self.token = []
-					return tkn
+					break
 		
-		return ''.join(self.token) if len(self.token) > 0 else None
+		# char is newline
+		# TODO: fix newlines getting tangled with numbers
+		if re.match(self.syntax.r_newline, char):
+			self.nchar = 0
+			self.nline = self.nline + 1
+		else:
+			self.nchar 	= self.nchar + 1
+			
+		token 		= ''.join(self.token)
+		info 		= '%s:%s' % (self.nline,self.nchar)
+				
+		return (info,token) if len(token) > 0 else (info,None)
 
 	def next(self):
 	
@@ -84,7 +152,7 @@ class Lexer(object):
 		
 		while True:
 			# get next symbol
-			symbol = self.scan()
+			info,symbol = self.scan()
 
 			# EOF				
 			if symbol is None:
@@ -96,7 +164,7 @@ class Lexer(object):
 				if regexp is None:
 					continue
 
-				if re.match(regexp, symbol, re.M):
+				if re.match(regexp, symbol):
 					word.append(symbol)
 					# move forward in tree
 					tree = tree[regexp]
@@ -115,36 +183,56 @@ class Lexer(object):
 				tree = self.syntax.symbols
 				continue
 
-			return (''.join(word), tree)
+			return (info,''.join(word), tree)
 		
 class Parser(object):
 	
-	def __init__(self, syntax, source, is_file=True):
-		self.syntax = syntax
-		self.lexer 	= Lexer(syntax, source, is_file)
+	def __init__(self, lang, source, is_file=True):
+		self.lang	= lang
+		self.lexer 	= Lexer(lang, source, is_file)
+		self.tree 	= []
 		
 	def verbatim(self, stop):
 		verbatim = []
 		while True:
-			symbol = self.lexer.scan()
-			if symbol == stop or symbol is None:
+			symbol = self.lexer.next()
+			
+			# stop capturing verbatim on stop character
+			# or EOF
+			if not symbol or symbol[1] == stop or symbol[1] is None:
 				break
 			else:
-				verbatim.append(symbol)
+				verbatim.append(symbol[1])
 			
 		return verbatim
+	
+	def statement(self, token):
+		term = token[2](token[0])
+		print isinstance(term, self.lang.Identifier)
 		
 	def parse(self):
 		while True:
 			n = self.lexer.next()
 			if n is False:
 				break
-			print n
-
-
 			
-						
-		
-
+			# fast forward through comment blocks
+			if n[2] == '[comment_block_open]':
+				self.verbatim('*/')
+			elif n[2] == '[comment_line]':
+				self.verbatim('\n')
+			elif n[2] == '[newline]':
+				pass
+			elif n[2] == '[space]':
+				pass
+			elif n[2] == '[tab]':
+				pass
+			elif n[2] == '[keyword]':
+				pass
+			else:	
+				self.statement(n)
+			
+			
+			
 lex = Parser(Doubletalk(), 'test.dtk')
 lex.parse()
